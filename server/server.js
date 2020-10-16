a1acb6d51137fb115079206ddd08090bc645b4b1
Carlos-Kaspa <carloslimatwg@gmail.com>
2020-10-16 14:10:34 -0300

    chore: muro das lamentações

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
-dconst express = require('express');
+const express = require('express');
 const cors = require('cors'); // pacote para adicionar headers e evitar o erro de cors na comunicação do live erver com o express
 const { networkInterfaces } = require('os'); //coisa do cors
+const monk = require('monk');
 
 
+
+const db = monk('localhost/MDL')
+const posts = db.get('posts');
 const app = express();
 
 app.use(cors()); //invoca o pacote do cors para o express
@@ -32,16 +36,31 @@ function isValid(post){
     //importante passar para toString() para evitar injeções de código e .trim() para tirar os espaços vazios do início e fim
 }
 
+app.get('/posts', (req,res) => {
+    posts.find().then(posts =>{
+        res.json(posts);
+    })
+})
+
 app.post('/posts', (req,res) => {
 
     if(isValid(req.body)){ //validação para saber se o post tem conteúdo
         const post = {
 
-            nome:req.body.name.toString(),
-            message: req.body.message.toString()
+            name:req.body.name.toString(),
+            message: req.body.message.toString(),
+            created_date: new Date()
         }
 
-        console.log(post)//debug
+        console.log(post)
+        
+
+        posts.insert(post).then(createdPost => {
+            res.json(createdPost)
+           
+        });
+
+       
 
     }else{
         res.status(777) //mensagem de erro caso não tiver
@@ -49,7 +68,5 @@ app.post('/posts', (req,res) => {
             message: 'Nome e mensagem são obrigatórios, seu sabichão!'
         })
     }
-  
-
 })
 
